cfc6515d6667101a99c89a2d2d185be83cf3de28
voidzero222 <voidzeroxdd@proton.me>
2025-11-07 17:31:53 +0100

    ich hasse aes

--- scripts/encryption.gd
+++ scripts/encryption.gd
@@ -2,21 +2,36 @@ extends Node
 class_name Encryption
 
 func get_sbox_value(hex_in:String) -> String:
-	#line edit in datagrid, wenn änderung dann erste stelle als zeile in sbox verwenden, zweite als index, 
-	#nimmt einzelne hexadezimalzahlen
 	var x:int = hex_in[0].hex_to_int()
 	var y:int = hex_in[1].hex_to_int()
 	return Constants.SBOX[x][y]
-func rot_word(vector_in:Vector4i, times:int =1) -> Vector4i:
+func rot_word(array_in:PackedStringArray, times:int =1) -> PackedStringArray:
 	if times == 0:
-		return vector_in
-	return rot_word(Vector4i(vector_in.y,vector_in.z,vector_in.w,vector_in.x),times -1)
+		return array_in
+	return rot_word(PackedStringArray([array_in[1],array_in[2],array_in[3],array_in[0]]),times -1)
 func generate_round_keys():
 	#gehirnfick
 	pass
-func shift_rows():
-	pass
-func mix_columns():
-	pass
+func shift_rows(matrix_in: Array[PackedStringArray]) -> Array[PackedStringArray]:
+	matrix_in = matrix_in.duplicate(true)
+	for i in 4:
+		var row:PackedStringArray = matrix_in[i]
+		row = rot_word(row,i)
+		matrix_in[i] = row
+	return matrix_in
+func mix_columns(matrix_in:Array[PackedStringArray]) -> Array[PackedStringArray]:
+	matrix_in = matrix_in.duplicate()
+	for i in 4:
+		var column:Array[int] = [matrix_in[0][i].hex_to_int(),matrix_in[1][i].hex_to_int(),
+		matrix_in[2][i].hex_to_int(),matrix_in[3][i].hex_to_int()]
+		for row in Constants.GALOIS_FIELD:
+			var mixed_column:Array[int] = [column[0]*row[0]%256,column[1]*row[1]%256,
+			column[2]*row[2]%256,column[3]*row[3]%256]
+			matrix_in[0][i] = "%x" % mixed_column[0]
+			matrix_in[1][i] = "%x" % mixed_column[1]
+			matrix_in[2][i] = "%x" % mixed_column[2]
+			matrix_in[3][i] = "%x" % mixed_column[3]
+	return matrix_in
+	
 func add_round_key():
 	pass
